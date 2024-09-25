5140fb368e074b291ba96a138b2e7edfcd2be989
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-09-25 19:19:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ provider "aws" {
 resource "aws_instance" "terrateam-instance" {
   ami = "ami-0e86e20dae9224db8" 
   instance_type = "t2.micro"
+  subnet_id = "subnet-02efa144df0a77c13" 
 
   tags = {
     Name        = "TerraTeamInitialInstance"
